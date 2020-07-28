699101d61e7b10d7d01f9214de65616279449da5
SpookyScaryStudios <thomaslcave@gmail.com>
2020-07-28 08:24:16 -0700

    Added movement to sandbox

--- Sandbox/Source/SandboxApp.cpp
+++ Sandbox/Source/SandboxApp.cpp
@@ -2,30 +2,64 @@
 
 using namespace Jem;
 
-void SpinnySystem(ECSManager* world) {
-    static double angle = 0;
-    angle += 0.2;
-    
-    for (Entity entity : world->GetEntitiesWith<TransformComponent>()) {
+struct PhysicsComponent {
+    Vector2d velocity  = { 0.0, 0.0 };
+    Vector2d drag      = { 0.0, 0.0 };
+    //Vector2d gravity   = { 0.0, 0.0 };
+};
+
+void PhysicsSystem(ECSManager* world) {
+    for (Entity entity : world->GetEntitiesWith<PhysicsComponent, TransformComponent>()) {
+        PhysicsComponent&   physics   = world->GetComponent<PhysicsComponent>(entity);
         TransformComponent& transform = world->GetComponent<TransformComponent>(entity);
 
-        if (angle == 360) angle = 0;
+        if (physics.velocity.x < 0.0) physics.velocity.x += physics.drag.x;
+        if (physics.velocity.y > 0.0) physics.velocity.y -= physics.drag.y;
+        if (physics.velocity.y < 0.0) physics.velocity.y += physics.drag.y;
+        if (physics.velocity.x > 0.0) physics.velocity.x -= physics.drag.x;
 
-        transform.rotation = angle;
+        transform.position += physics.velocity;
+    }
+}
+
+struct PlatformerControllerComponent {};
+
+void PlatformerControllerSystem(ECSManager* world) {
+    for (Entity entity : world->GetEntitiesWith<PlatformerControllerComponent, PhysicsComponent>()) {
+        PhysicsComponent& physics = world->GetComponent<PhysicsComponent>(entity);
+
+        if (Input::IsKeyPressed(KeyCode::KEY_UP))    physics.velocity.y -= 2;
+        if (Input::IsKeyPressed(KeyCode::KEY_DOWN))  physics.velocity.y += 2;
+        if (Input::IsKeyPressed(KeyCode::KEY_LEFT))  physics.velocity.x -= 2;
+        if (Input::IsKeyPressed(KeyCode::KEY_RIGHT)) physics.velocity.x += 2;
+    }
+}
+
+void SpinnySystem(ECSManager* world) {
+    for (Entity entity : world->GetEntitiesWith<TransformComponent, PhysicsComponent>()) {
+        TransformComponent& transform = world->GetComponent<TransformComponent>(entity);
+        PhysicsComponent&   physics   = world->GetComponent<PhysicsComponent>(entity);
+
+        transform.rotation += physics.velocity.x / 10;
+        if (transform.rotation == 360) transform.rotation = 0;
     }
 }
 
 class Sandbox : public Game {
 public:
-    Sandbox() : Game("Sandbox", 500, 500) {
+    Sandbox() : Game("Sandbox", 1600, 900) {
         texture = new Texture("Assets/foo.bmp");
         world.RegisterComponentType<TextureComponent>();
         world.RegisterComponentType<TransformComponent>();
+        world.RegisterComponentType<PhysicsComponent>();
+        world.RegisterComponentType<PlatformerControllerComponent>();
 
         for (int i = 10; i >= 0; i--) {
             Entity entity = world.CreateEntity(i);
             world.AddComponent<TextureComponent>(entity, { texture });
-            world.AddComponent<TransformComponent>(entity, { {125, double(20 + 20 * i) }, { 250, 250 } });
+            world.AddComponent<TransformComponent>(entity, { Vector2d(Window::GetWidth() / 2 - 125, Window::GetHeight() / 2 - 125 + 20 * i ), { 250, 250 } });
+            world.AddComponent<PhysicsComponent>(entity, { {}, { 1, 1 },});
+            world.AddComponent<PlatformerControllerComponent>(entity, {});
         }
     }
 
@@ -33,6 +67,8 @@ public:
         Renderer::Clear();
         SpinnySystem(&world);
         RenderSystem(&world);
+        PlatformerControllerSystem(&world);
+        PhysicsSystem(&world);
         Renderer::Refresh();
     }
 
